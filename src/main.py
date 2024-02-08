c40051e901b552822ae460441be34904fb928ca0
3Dori <chelvyixue@gmail.com>
2024-02-08 23:33:35 +0100

    Add PowerControl.receive

--- src/main.py
+++ src/main.py
@@ -6,15 +6,11 @@ from power_control import PowerControl
 
 if __name__ == '__main__':
     K: int = 20
-    D: int = 2
     MAX_POW: float = 1.0
     SIGMA: float = 1.0
-    h: np.array = np.random.normal(0.0, 1.0, (K, D))
-    h_norm: np.array = np.linalg.norm(h, axis=1)
-    p_max: np.array = np.full(K, MAX_POW)
 
-    pc = PowerControl(h_norm, p_max, SIGMA)
-    p_star, eta_star = pc.compute_optimal_p_and_eta(plot=True)
+    pc = PowerControl(K, MAX_POW, SIGMA)
+    print(pc.receive(np.random.normal(0, 1, (K, 10, 20))))
 
     # b: np.array = h_norm * np.sqrt(p_star) / np.sqrt(eta_star)
     # plt.bar(np.arange(1, K+1), b)
--- src/power_control.py
+++ src/power_control.py
@@ -10,13 +10,31 @@ class PowerControl:
     'Optimized Power Control for Over-the-Air Computation in Fading Channels'
     by Xiaowen Gao et al.
     """
-    def __init__(self, h_norm: np.array, p_max: np.array, sigma: np.float32) -> None:
-        self.k: int = len(h_norm)
-        self.h_norm = h_norm
-        self.p_max = p_max
+    def __init__(self, k: int, p_max: float, sigma: float) -> None:
+        self.k = k
+        self.h = self._generate_h(k)
+        self.h_norm = np.linalg.norm(self.h, axis=1)
+        self.p_max = np.full(k, p_max)
         self.sigma = sigma
 
-    def compute_optimal_p_and_eta(self, plot: bool = True) -> Tuple[np.array, np.float32, np.array]:
+        self.p_star, self.eta_star = self.compute_optimal_p_and_eta()
+        self.b = self.h_norm * np.sqrt(self.p_star) / np.sqrt(self.eta_star)
+        self.k_sqrt_eta_star = self.k * np.sqrt(self.eta_star)
+
+    @staticmethod
+    def _generate_h(k) -> np.array:
+        return np.random.normal(loc=0, scale=np.sqrt(2)/2, size=(k, 2)).view(np.complex128)
+    
+    def _generate_z(self, shape) -> np.array:
+        return np.random.normal(0, self.sigma, shape)
+    
+    def receive(self, w) -> np.array:
+        assert w.shape[0] == self.k
+        x: np.array = np.sum(self.b[:,np.newaxis,np.newaxis] * w, axis=0)
+        z: np.array = self._generate_z(shape=x.shape)
+        return (x + z) / self.k_sqrt_eta_star
+
+    def compute_optimal_p_and_eta(self, plot: bool = False) -> Tuple[np.array, np.float32, np.array]:
         sorted_p_h_sqaured: np.array = self.p_max * (self.h_norm ** 2)
         sorted_index: np.array = np.argsort(sorted_p_h_sqaured)
         rank: np.array = np.argsort(sorted_index)
@@ -24,23 +42,23 @@ class PowerControl:
         p_star, eta_star = self._compute_optimal_p_and_eta_with_sorted_p_h_sqaured(sorted_index, plot)
         return p_star[rank], eta_star
 
-    def _compute_optimal_p_and_eta_with_sorted_p_h_sqaured(self, sorted_index: np.array, plot: bool):
+    def _compute_optimal_p_and_eta_with_sorted_p_h_sqaured(self, sorted_index: np.array, plot: bool) -> Tuple[np.array, np.float32, np.array]:
         p_max: np.array = self.p_max[sorted_index]
         h_squared: np.array = (self.h_norm ** 2)[sorted_index]
         p_h_sqaured = p_max * h_squared
 
         sum_1_to_i_pi_hi_squared: np.array = np.cumsum(p_h_sqaured)
         sum_1_to_i_sqrt_pi_hi: np.array = np.cumsum(np.sqrt(p_h_sqaured))
-        eta: np.array = ((self.sigma ** 2 + sum_1_to_i_pi_hi_squared) / sum_1_to_i_sqrt_pi_hi) ** 2
-        k_star: np.int64 = np.argmax(eta < p_h_sqaured)
-        eta_star: np.float32 = eta[k_star]
-        p_star: np.array = np.where(eta >= p_h_sqaured, p_max, eta_star / h_squared)
+        eta_tilde: np.array = ((self.sigma ** 2 + sum_1_to_i_pi_hi_squared) / sum_1_to_i_sqrt_pi_hi) ** 2
+        k_star: np.int64 = np.argmax(eta_tilde < p_h_sqaured)
+        eta_star: np.float32 = eta_tilde[k_star]
+        p_star: np.array = np.where(eta_tilde >= p_h_sqaured, p_max, eta_star / h_squared)
 
         if plot:
             k: np.array = np.arange(1, self.k+1)
             width = 0.3
             plt.bar(k, p_h_sqaured, width=width)
-            plt.bar(k + width, eta, width=width)
+            plt.bar(k + width, eta_tilde, width=width)
             plt.show()
 
             plt.bar(k, p_star, width=width)
